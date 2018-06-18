b0ee42b869312b4a8e161f5c6893cefcf363d29d
Masataka Shintoku <masataka.shintoku@gmail.com>
2018-06-18 18:22:50 +0900

    Added body parser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const router = require('./route/index');
 
 const app = express();
+app.use(bodyParser.json());
 app.use('/api', router);
 
 // TODO: you need to implement server routing!!
