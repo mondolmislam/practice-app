95d06a939bb6fc0d5bebaacebc66e40efb24a6f0
mondolmislam <mondolmislam@gmail.com>
2019-02-04 00:30:16 +0600

    under components->users->user.js added a static async method for data insert and using usersDAL.js insert method

--- components/users/user.js
+++ components/users/user.js
@@ -3,6 +3,14 @@ const { camelizeKeys } = require('humps');
 const usersDAL = require('./usersDAL');
 
 class User {
+  static async insert (userData){
+      // Add the user.
+      console.log(userData);
+      let user = await usersDAL.insert(userData);
+
+      // return the user object
+      return user;
+  }
   // TODO: you need to implement insert method in User class.
   static async select() {
     try {
